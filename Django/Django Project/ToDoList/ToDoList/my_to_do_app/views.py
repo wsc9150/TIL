79a2581da46f3775903798fe1c4ff464af349f7d
WooSeok Choi <wsc9150@gmail.com>
2020-09-23 19:09:41 +0900

    Modify ToDoList

--- Django/Django Project/ToDoList/ToDoList/my_to_do_app/views.py
+++ Django/Django Project/ToDoList/ToDoList/my_to_do_app/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
+
+
+
+def index(request) :
+    todos = Todo.objects.all()
+    context = {'todos' : todos}
+
+    return render(request, 'my_to_do_app/index.html', context)
+
+
+def createTodo(request) :
+    user_input_str = request.POST['todoContent']
+
+    new_todo = Todo(content = user_input_str)
+    new_todo.save()
+
+    return redirect('index')
+
+
