98fdb3346c28d3c667f7d4be1747c5b6ef398a71
WooSeok Choi <wsc9150@gmail.com>
2020-10-20 18:29:12 +0900

    Modify .gitignore and Add Java, visualization

---
+++ Java/Algorithm/Search/src/linear_search01.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class linear_search01 {
+
+	public static void main(String[] args) {
+		int[] x = {6, 4, 3, 2, 1, 2, 8};
+		Scanner s = new Scanner(System.in);
+		
+		int k = s.nextInt();
+		
+		for (int i = 0; i < x.length; i++) {
+			System.out.println(x[i]);
+		}
+	}
+
+}
