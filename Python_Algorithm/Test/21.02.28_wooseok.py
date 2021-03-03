db9057065420b3c59cb0d952fb88ccb39799bb09
WooSeok Choi <wsc9150@gmail.com>
2021-03-03 15:07:15 +0900

    21.03.03 | mod coding test

--- Python_Algorithm/Test/21.02.28_wooseok.py
+++ Python_Algorithm/Test/21.02.28_wooseok.py
@@ -1,3 +1,4 @@
+# 신규 아이디 추천
 def solution1(new_id):
     answer = ''
 
@@ -57,51 +58,56 @@ new_id = "...!@BaT#*..y.abcdefghijklm"
 # print(solution1(new_id))
 
 
+
+# 메뉴 리뉴얼
 import itertools
 
 
 def solution2(orders, course):
     answer = []
 
-    menu_list = []
-
-    for order in orders:
-        menu_list.extend(order)
-
-    menu_list = list(set(menu_list))
-    menu_list.sort()
-
     for i in course:
-        part = list(map(''.join, itertools.combinations(menu_list, i)))
-        count_list = []
+        part_list = []
 
-        for p in part:
-            count = 0
+        for order in orders:
+            order = list(order)
+            order.sort()
 
-            for order in orders:
+            part_list.extend(list(map(''.join, itertools.combinations(order, i))))
 
-                for k in p :
-                    if k not in order :
-                        break
-                else :
-                    count += 1
+        dict = {}
+        for part in part_list :
+            if part not in dict.keys() :
+                dict[part] = 1
+            else :
+                dict[part] += 1
 
-            count_list.append(count)
+        dict_list = sorted(dict.items(), key = lambda x : x[1], reverse = True)
 
-        _max = max(count_list)
+        if dict_list :
+            max_value = dict_list[0][1]
 
-        if _max <= 1 :
-            continue
-        else :
-            for j in range(len(count_list)) :
-                if count_list[j] == _max :
-                    answer.append(part[j])
+            if max_value > 1 :
+                for j in dict_list :
+                    if j[1] == max_value :
+                        answer.append(j[0])
 
     answer.sort()
 
     return answer
 
 
-orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+orders = ["XYZ", "XWY", "WXA"]
 course = [2, 3, 4]
 print(solution2(orders, course))
+
+# 시간초과 원인
+
+# 애초에 단품 메뉴들의 모든 부분집합을 만들어놓고
+# 그 부분집합을 또 나누어서 orders 와 일일이 비교를 하려했던 것이 시간초과의 원인...
+# 위에 말한대로 코드를 짜면 최소 4중 반복문이 나오기 때문에
+# 채점에서 두 개의 테스트 케이스는 시간초과이고, 통과된 케이스중에서 최대로 걸린 시간은 53.86ms
+
+# orders 에 포함된 메뉴들만으로 부분집합을 만들고, 개수를 측정하는 것이 훨씬 효율적이다.
+# combinations 함수와 not in 함수를 제외해도 2중 반복문밖에 나오지 않는다.
+# 테스트 케이스중 최대로 걸린 시간이 2.51ms로, 정말 많이 줄어든다.
