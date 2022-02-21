178bac67683aff8b454f3151059d21e2acbc5ffe
Choi Wooseok <wsc9150@gmail.com>
2022-02-21 21:15:22 +0900

    22.02.21 | add kakao test

---
+++ Python_Algorithm/Programmers/Kakao/22.02.04_wooseok.py
@@ -0,0 +1,34 @@
+# 실패율
+# https://programmers.co.kr/learn/courses/30/lessons/42889
+
+
+def solution(N, stages):
+    answer = []
+
+    success_dict = {}
+
+    for i in range(1, N + 1):
+        sum_value = 0
+
+        for stage in stages :
+            if stage >= i :
+                sum_value += 1
+
+        in_stage = stages.count(i)
+
+        if sum_value != 0 :
+            success_dict[i] = in_stage / sum_value
+        else :
+            success_dict[i] = 0
+
+    sorted_list = sorted(success_dict.items(), key = lambda x : -x[1])
+
+    for i in sorted_list :
+        answer.append(i[0])
+
+    return answer
+
+
+N = 5
+stages = [2, 1, 2, 6, 2, 4, 3, 3]
+print(solution(N, stages))
